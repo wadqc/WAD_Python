ad980f9d0770fe16b1b17b5b057e37d4cbde1883
asch99 <aschilham@gmail.com>
2016-08-26 16:06:32 +0200

    sync with wad2.0; python3 compatible; license

--- Plugins/MG/Mammo_Flatfield/QCMammo_constants.py
+++ Plugins/MG/Mammo_Flatfield/QCMammo_constants.py
@@ -10,7 +10,6 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 String constants for usage in QCMammo
 """
